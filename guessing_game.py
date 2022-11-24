d10a622261350702be035800fabb98fe470c486c
malor <malor@orsted.com>
2022-11-24 11:31:47 +0100

    guessing game implemented

--- guessing_game.py
+++ guessing_game.py
@@ -1,6 +1,19 @@
+import random
+
+
 def guessing_game():
-    pass
+    num = random.randint(1, 101)
+    while True:
+        guess = int(input('Guess between 1 and 100: '))
+        if guess == num:
+            print('Congrats')
+            break
+        if guess > num:
+            print('Too high :(')
+        elif guess < num:
+            print('Too low :(')
 
 
+guessing_game()
 """Number guessing game"""
 
