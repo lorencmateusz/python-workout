4764379c25736f2ca48376f9ff464ec4a7973906
malor <malor@orsted.com>
2022-11-25 15:56:06 +0100

    feat: exercise 6

--- pig_latin.py
+++ pig_latin.py
@@ -1,5 +1,5 @@
 def pig_latin(string):
-    if string[0] in 'aeiou':
+    if string[0].lower() in 'aeiou':
         return string + 'way'
     else:
         return string[1:] + string[0] + 'ay'
@@ -7,3 +7,10 @@ def pig_latin(string):
 
 print(pig_latin('python'))
 print(pig_latin('air'))
+
+
+def pig_latin_sentence(sentence):
+    return ' '.join([pig_latin(x) for x in sentence.split(' ')])
+
+
+print(pig_latin_sentence('this is a test translation'))
